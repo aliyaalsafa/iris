523e391a81cd9621d7a67306c48b6a384336d976
Nikita Tyunyayev <nikita.tyunyayev@uclouvain.be>
2026-07-02 12:49:24 +0200

    cleaning

--- run-drop-intel.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-source "${SCRIPT_DIR}/env-drop.sh"
-build
-exec "${SCRIPT_DIR}/run-drop.sh" "$@"
